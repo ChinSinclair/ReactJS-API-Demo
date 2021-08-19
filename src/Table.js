394839e412dba17db55d6fb27c363cb0852359db
ChinSinclair <sinclair_chin@hotmail.com>
2021-08-19 12:37:48 +1000

    Removed redundant variables

--- src/Table.js
+++ src/Table.js
@@ -231,7 +231,6 @@ export default function TableDisplay() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const [data, setData] = useState([]);
   var rows = [];
   var tempRows = [];
 
